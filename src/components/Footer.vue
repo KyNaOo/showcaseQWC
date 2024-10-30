b9579fd442dbaa92a433549a84dbfe56e16a806c
KyNaOo <qinhao.wu122@gmail.com>
2024-10-30 16:36:05 +0100

    feat: cleaning + set up tailwindcss

---
+++ src/components/Footer.vue
@@ -0,0 +1,20 @@
+<template>
+  <footer class="bg-gray-800 text-white py-4">
+    <div class="container mx-auto text-center">
+      <p>&copy; 2024 Qindi Wu Consulting. All rights reserved.</p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "CopyrightFooter",
+};
+</script>
+
+<style scoped>
+footer {
+  font-size: 14px;
+  background-color: #cdbfbf;
+}
+</style>
