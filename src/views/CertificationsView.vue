32748113fbf0d38ea3fe1ebfb6a9f6d49b7c90bc
KyNaOo <qinhao.wu122@gmail.com>
2024-11-07 15:29:51 +0100

    feat: importing images

--- src/views/CertificationsView.vue
+++ src/views/CertificationsView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-neutral-500 flex flex-col items-center justify-center">
     <div
-      class="bg-white rounded-lg shadow-md p-4 mb-6 w-full max-w-md"
+      class="bg-white rounded-lg shadow-md p-6 mb-8 w-full max-w-lg"
       v-for="cert in certifications"
       :key="cert.title"
     >
@@ -9,10 +9,10 @@
         <div class="w-1/3">
           <img :src="cert.imagePath" class="w-full h-auto" />
         </div>
-        <div class="w-2/3 pl-4">
-          <h2 class="text-lg font-bold mb-1">{{ cert.title }}</h2>
-          <p class="text-gray-600 mb-1">Earned in {{ cert.year }}</p>
-          <p class="text-sm">{{ cert.description }}</p>
+        <div class="w-2/3 pl-6">
+          <h2 class="text-xl font-bold mb-2">{{ cert.title }}</h2>
+          <p class="text-gray-600 mb-2">Earned in {{ cert.year }}</p>
+          <p class="text-base">{{ cert.description }}</p>
         </div>
       </div>
     </div>
@@ -29,21 +29,21 @@ export default {
           year: 2022,
           description:
             "Demonstrates proficiency in building and deploying secure and scalable applications on the AWS platform.",
-          imagePath: "/images/aws-certified-developer.png",
+          imagePath: "../public/aws.png",
         },
         {
           title: "Kubernetes Certified Administrator",
           year: 2021,
           description:
             "Validates the skills and knowledge to configure, manage, and operate production-grade Kubernetes clusters.",
-          imagePath: "/images/kubernetes-certified-admin.png",
+          imagePath: "../public/kubernetes.png",
         },
         {
           title: "Google Cloud Certified Professional",
           year: 2023,
           description:
             "Certifies the ability to design, develop, and manage robust, secure, and scalable solutions on the Google Cloud platform.",
-          imagePath: "/images/google-cloud-certified.png",
+          imagePath: "../public/aws2.png",
         },
       ],
     };
@@ -51,6 +51,4 @@ export default {
 };
 </script>
 
-<style>
-/* No custom styles needed, using Tailwind CSS utility classes */
-</style>
+<style></style>
