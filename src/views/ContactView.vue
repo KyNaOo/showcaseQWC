7e3e20c640d626732fa3ba87ece49947812b546b
KyNaOo <qinhao.wu122@gmail.com>
2024-11-13 14:35:50 +0100

    feat: removing confidential datas

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -5,70 +5,9 @@
         <!-- Contact Information Card -->
         <div class="bg-white shadow-md rounded-lg p-8">
           <h2 class="text-2xl font-bold mb-4">Contact me</h2>
-          <div class="space-y-4">
-            <div class="flex items-center space-x-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6 text-gray-500"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"
-                />
-              </svg>
-              <span class="text-gray-700 font-medium">+1 (555) 555-5555</span>
-            </div>
-            <div class="flex items-center space-x-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6 text-gray-500"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                />
-              </svg>
-              <span class="text-gray-700 font-medium">w.qindi@hotmail.fr </span>
-            </div>
-            <div class="flex items-center space-x-4">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                class="h-6 w-6 text-gray-500"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
-                />
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
-                />
-              </svg>
-              <span class="text-gray-700 font-medium"
-                >123 Main St, Anytown USA</span
-              >
-            </div>
-          </div>
+          <div class="space-y-4"></div>
           <div class="mt-6">
             <a
-              href="mailto:info@qindiwu.com?subject=Inquiry from Website"
               class="bg-[#CDBFBF] hover:bg-gray-200 text-black font-medium py-2 px-4 rounded-md transition-colors"
             >
               Send Email
